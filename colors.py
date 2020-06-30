c41c2e71ab1f62408f6455fc3c623f196db6dc07
aiglematth <aiglematth@protonmail.com>
2020-06-30 16:49:24 +0200

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,9 @@
+#Auteur --> aiglematth
+
+#ICI VOUS METTEZ LES COULEURS DE VOTRE INTERFACE
+bgColor          = "light gray"
+instructionColor = "maroon1"
+registreColor    = "blue2"
+commentaireColor = "brown"
+clefColor        = "purple"
+strColor         = "green4"
